7dd419a26ec1401f916a30c905bfca71426d6b89
Nicholas Payne <ndpayne92@comcast.net>
2016-01-22 00:28:23 -0800

    Began flow control to view list of recipes

--- CalorieCalculator.py
+++ CalorieCalculator.py
@@ -1,17 +1,66 @@
-# This project is intended to calculate the calories for a list of ingredients in a CSV spreadsheet
-
 import bs4, csv, os, requests
+
+def introduction():
+    os.chdir('c:\\users\\ndpayne\\calorieScraper')
+
+    print('Hello! Welcome to your recipe program.\n\n' \
+          'Enter 1 to view the list of existing recipes.\n' \
+          'Enter 2 to add a new recipe.\n'
+          'Enter 3 to search for a recipe by ingredient.')
+
+def initUserInput():
+    
+    invalidEntry = True
+    while invalidEntry:
+        userChoice = input()
+        
+        if userChoice == '1':
+            print('Here is the list of recipes you\'ve created.')
+            recipeList = listRecipes()
+            for recipe in recipeList:
+                print(recipe)
+            
+            print('Type in the exact name of the file you would like to view including filetype.')
+            viewSpecRecipe(recipeList)
+            
+            invalidEntry = False
+            
+        elif userChoice == '2':
+            print('You entered 2.')
+            invalidEntry = False
+            
+        elif userChoice == '3':
+            print('You entered 3.')
+            invalidEntry = False
+        else:
+            print('You didn\'t enter a valid character, please try again.')
+
+def listRecipes():
+    for folderName, subfolders, filenames in os.walk('recipe_repo'):
+        return filenames
+
+def viewSpecRecipe(recipeList):
+    while True:
+        recipeNameInput = input()
+        if (recipeNameInput) not in recipeList:
+            print('What you entered isn\'t a valid recipe. Please enter a recipe name again.')
+        else:
+            print('You did it!')
+            break
+
+introduction()
+initUserInput()
+
+print('Exiting program.')
+
+'''
+
+
 
 # Open recipe file
 
-# print('What is the name of the recipe?')
-# recipeName = input()
-
-# recipeInFile = open('c:\\users\\ndpayne\\MyPythonScripts\\' + recipeName + '.csv')
-
-os.chdir('c:\\users\\ndpayne\\calorieScraper')
 recipeInFile = open('Dense_Salad_With_Lemon_Honey_Dressing.csv')
-recipeOutFile = open('Dense_Salad_With_Lemon_Honey_Dressing_Output.csv', 'w', newline='')
+recipeOutFile = open('recipe_repo\\Dense_Salad_With_Lemon_Honey_Dressing.csv', 'w', newline='')
 recipeReader = csv.reader(recipeInFile)
 recipeWriter = csv.writer(recipeOutFile)
 
@@ -32,7 +81,7 @@ for row in recipeReader:
 
 # Scrape number of calories from google search
 
-    caloriesInt = caloriesFromGoogle[0].text.split(' ', 1)[0] # strip 'calories' from selected string
+    caloriesInt = caloriesFromGoogle[0].text.split(' ', 1)[0] # 65 calories --> 65
     
     
 # Write the cell next to next to ingredient and multiply by serving size
@@ -52,3 +101,5 @@ recipeInFile.close()
 recipeOutFile.close()
 
 print(calorieSum)
+
+'''
